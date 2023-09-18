50fb69a61d804afed87e72d2e73b3c169874fac3
nikhmurthy <namurthy@usc.edu>
2023-09-18 16:07:04 -0700

    updated fields

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,30 +1,79 @@
-import { withAuthenticator } from "@aws-amplify/ui-react";
+import { Amplify } from 'aws-amplify';
 
-export function getServerSideProps() {
-  const renderedAt = new Date();
-  const formattedBuildDate = renderedAt.toLocaleDateString("en-US", {
-    dateStyle: "long",
-  });
-  const formattedBuildTime = renderedAt.toLocaleTimeString("en-US", {
-    timeStyle: "long",
-  });
-  return {
-    props: {
-      renderedAt: `${formattedBuildDate} at ${formattedBuildTime}`,
-    },
-  };
-}
+import { Authenticator, useAuthenticator } from '@aws-amplify/ui-react';
+import { CheckboxField, TextField } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
-function Home({ signOut, user, renderedAt }) {
+import awsExports from '../../src/aws-exports';
+Amplify.configure(awsExports);
+
+export default function Auth() {
+  const formFields = {
+    signUp: {
+      'custom:first_name': {
+        order: 1,
+        label: "First Name",
+        placeholder: "Enter your first name",
+        isRequired: true,
+      },
+      'custom:last_name': {
+        order: 2,
+        label: "Last Name",
+        placeholder: "Enter your last name",
+        isRequired: true,
+      },
+      email: {
+        order: 3,
+        placeholder: "Enter your email"
+      },
+      phone_number: {
+        order: 4,
+        label: "Phone Number",
+        placeholder: "Enter your phone number",
+      },
+      'custom:role': {
+        order: 5,
+        label: "Role",
+        placeholder: "Enter your role — either supplier or client",
+        isRequired: true,
+      },
+      'custom:company': {
+        order: 6,
+        label: "Company",
+        placeholder: "Enter your company name",
+        isRequired: true,
+      },
+      'custom:company_position': {
+        order: 7,
+        label: "Position",
+        placeholder: "Enter your position",
+        isRequired: true,
+      },
+      'custom:location': {
+        order: 8,
+        label: "Location",
+        placeholder: "Enter your location",
+        isRequired: true,
+      },
+      password: {
+        order: 9,
+        placeholder: "Enter your password"
+      },
+      confirm_password: {
+        order: 10,
+        placeholder: "Please confirm your password"
+      }
+    }
+  }
+
   return (
-    <div style={{ padding: 50 }}>
-      <h1>Logged in as {user.username}.</h1>
-      <div>
-        <button onClick={signOut}>Sign out</button>
-      </div>
-      <p>This page was server-side rendered on {renderedAt}.</p>
-    </div>
-  );
+    <Authenticator formFields={formFields}>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
+  )
 }
-
-export default withAuthenticator(Home);
